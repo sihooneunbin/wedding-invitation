e44833bc0df6749019d24fad2e491a8bc5cb6eb2
sihooneunbin <eunbin1231@gmail.com>
2026-09-08 11:58:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -400,12 +400,3 @@ if (dday) {
   opacity:1;
   transform:translateY(0);
 }
-window.addEventListener("load", () => {
-  document.querySelectorAll(".section").forEach((element) => {
-    element.classList.add("visible");
-  });
-
-  document.querySelectorAll(".hero-photo, .ending-photo").forEach((element) => {
-    element.classList.add("visible");
-  });
-});
