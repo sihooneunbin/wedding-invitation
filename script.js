3d0745ee417f2e82001d84c8f30a0926f3aad216
sihooneunbin <eunbin1231@gmail.com>
2026-09-09 17:09:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+if ("scrollRestoration" in history) {
+  history.scrollRestoration = "manual";
+}
+
+window.addEventListener("pageshow", () => {
+  window.scrollTo(0, 0);
+});
 if (typeof Kakao !== "undefined") {
   Kakao.init("9b53925c5ae6be1717a4d4470199a081");
 }
