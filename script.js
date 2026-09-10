6f109c9d078ecf9c63e30a621958f5086679638e
sihooneunbin <eunbin1231@gmail.com>
2026-09-10 15:52:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -287,7 +287,7 @@ item.addEventListener("click", () => {
 
   lessButton.type = "button";
   lessButton.className = "story-less";
-  lessButton.textContent = "⌃ LESS";
+lessButton.innerHTML = '<span class="less-arrow">^</span> LESS';
 
   lessButton.addEventListener("click", (e) => {
 
